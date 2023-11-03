b8a197480868c5d95de66d8c7afa30bb0ed5809d
Ryan Breaux <ryanbreaux@uhual.com>
2023-11-02 18:57:35 -0700

    Update redirects

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -1,8 +1,19 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { viteStaticCopy } from 'vite-plugin-static-copy'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
   envDir: "./envDir/",
-  plugins: [react()],
+  plugins: [react(),
+  viteStaticCopy({
+    targets: [
+      {
+        src: '_redirects',
+        dest: './'
+      }
+    ]
+  })
+  ],
 });
